9300bad6ebfee6b297cccfbd5c81e117cafad98b
vaibhavsrh78@gmail.com <vaibhavsrh78@gmail.com>
2023-02-17 21:47:28 +0530

    another commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app =express();
 const getRoutes = require('./routes/getRoutes')
+//  const ngrok=require('ngrok');
 const PORT=3001;
 //middlewares
 app.use(express.json());
@@ -11,3 +12,12 @@ app.use('/api/v1',getRoutes);
 app.listen(PORT,()=>{
     console.log('server is running');
 });
+// (async function(){
+// const  url=await ngrok.connect({
+//     proto:'http',
+//     addr:PORT,
+//     authtoken;""
+
+// });
+// console.log(url);
+// });
--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -2,6 +2,7 @@ const  express=require('express');
 const routes=express.Router();
 const main=require('../scrapeFn/scrape');
 
+const path = require('path');
 routes.post('/indeed',async (req,res)=>{
     try{
         let skill=req.body.skill;
@@ -15,4 +16,14 @@ routes.post('/indeed',async (req,res)=>{
         return res.status(500).send(e);
     }
 })
+routes.get('/getData',async(req,res)=>{
+    try{
+        const jobs=path.join(__dirname,'..','job.json');
+        return res.status(200).sendFile(jobs);
+    }
+    catch(e){
+        return res.status(500).send(e);
+    }
+});
+
 module.exports=routes;
